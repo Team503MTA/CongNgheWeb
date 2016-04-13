0e53939bb9ebdd0316a251af91c59a56b7735310
VU HOANG HA <ladykillah.n0.1.th@gmail.com>
2016-04-14 01:30:37 +0700

    14/4/2016

---
+++ WebMusic/Controllers/LabelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMusic.Controllers
+{
+    public class LabelController : Controller
+    {
+        // GET: Label
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
